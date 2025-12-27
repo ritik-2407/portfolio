5e5aae19fd93be83a6dd42ca7697f1bd3e6a6930
Ritik <ritikyadav2426@gmail.com>
2025-12-27 17:58:33 +0530

    light mode bug test

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,9 +42,17 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${ubuntu.variable} ${lexend.variable} ${outfit.variable} font-body antialiased`}
+        // FIX APPLIED HERE:
+        // 1. bg-[#050505]: Forces the page background to be black (fixes the "weird" transparency issue).
+        // 2. text-zinc-100: Sets the default text color to light grey/white.
+        // 3. selection:bg-purple-500/30: Adds that nice purple highlight effect you had.
+        className={`${ubuntu.variable} ${lexend.variable} ${outfit.variable} font-body antialiased bg-[#050505] text-zinc-100 selection:bg-purple-500/30`}
       >
         <Analytics />
+        
+        {/* Note: If your icons/text still flip color in Light Mode, 
+           add the prop forcedTheme="dark" to this ThemeProvider tag.
+        */}
         <ThemeProvider>
           <Starfield />
           <div className="relative flex min-h-screen flex-col">
